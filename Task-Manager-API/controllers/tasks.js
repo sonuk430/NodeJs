7b02ac6e830ba3bfdd369ee8b89014e189361b46
Sonu Kumar <kumarsonuk430@gaiml.com>
2025-07-14 22:13:33 +0530

    all routes define

--- Task-Manager-API/controllers/tasks.js
+++ Task-Manager-API/controllers/tasks.js
@@ -1,5 +1,18 @@
 const getAllTasks = (req, res) => {
-  res.send("All Items...");
+  res.send("Get All Tasks...");
 };
 
-module.exports = { getAllTasks };
+const createTask = (req, res) => {
+  res.json(req.body);
+};
+const getTask = (req, res) => {
+  res.json({ id: req.params.id });
+};
+const updateTask = (req, res) => {
+  res.send("update task...");
+};
+const deleteTask = (req, res) => {
+  res.send("Delete task...");
+};
+
+module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
